e571bca123509e918005f23680e66ef1f9ebfd6f
Carla <carla.ad.muller@gmail.com>
2025-03-23 22:50:11 -0300

    Initial Commit

---
+++ Code/Level.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Level:
+    def __init__(selfself):
+        self.window = None
+        self.name = None
+        self.entity_list = None
+        self.entity_list = None
+
+    def run(self, ):
+        pass
