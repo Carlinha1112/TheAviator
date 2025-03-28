d469faca1f73b84df4ef1bb25afa115555a26475
Carla <carla.ad.muller@gmail.com>
2025-03-27 21:20:29 -0300

    Creating Mediator Pattern Enemy destruction

--- Code/Const.py
+++ Code/Const.py
@@ -7,6 +7,7 @@ COLOR_WHITE = (255, 255, 255)
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
+
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 1,
@@ -15,11 +16,23 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Player': 3,
-    'Enemy1Lv1': 3,
-    'Enemy2Lv1': 3,
-    'Enemy3Lv1': 3,
+    'Enemy1Lv1': 12,
+    'Enemy2Lv1': 14,
+    'Enemy3Lv1': 14,
 }
 
+ENTITY_HEALTH = {
+    'Level1Bg0': 999,
+    'Level1Bg1': 999,
+    'Level1Bg2': 999,
+    'Level1Bg3': 999,
+    'Level1Bg4': 999,
+    'Level1Bg5': 999,
+    'Player': 50,
+    'Enemy1Lv1': 999,
+    'Enemy2Lv1': 999,
+    'Enemy3Lv1': 999,
+}
 # M
 MENU_OPTION = ('NEW GAME', 'SCORE', 'EXIT')
 
